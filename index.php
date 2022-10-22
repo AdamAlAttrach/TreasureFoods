cfc5682947b861dbcc026180286be04881453794
AdamAlAttrach <adamalattrach@gmail.com>
2022-10-22 15:11:23 -0400

    Worked on overall layout

--- index.php
+++ index.php
@@ -15,28 +15,35 @@ if (isset($_SESSION["usersuid"])) {
         <div class="box">
             <img src="images/vegetables.jpg" alt="">
             <h3>vegetables</h3>
-            <p>up to 45% off</p>
+            <p>Tomatoes are in this section</p>
             <a href="#" class="btn">shop now</a>
         </div>
 
         <div class="box">
             <img src="images/fruits.jpg" alt="">
             <h3>fresh fruits</h3>
-            <p>up to 45% off</p>
+            <p>Have a taste of our delicious apples!</p>
             <a href="#" class="btn">shop now</a>
         </div>
 
         <div class="box">
             <img src="images/cereal.jpg" alt="">
             <h3>dairy products</h3>
-            <p>up to 45% off</p>
+            <p>All part of a nutritious breakfast!</p>
             <a href="#" class="btn">shop now</a>
         </div>
 
         <div class="box">
             <img src="images/Butcher.jpg" alt="">
             <h3>fresh meat</h3>
-            <p>up to 45% off</p>
+            <p>For the meatlovers!</p>
+            <a href="#" class="btn">shop now</a>
+        </div>
+
+        <div class="box">
+            <img src="images/winecellar.jpg" alt="">
+            <h3>Captain's Wine Cellar</h3>
+            <p>Get your own bottle of Caberlot before we run out!</p>
             <a href="#" class="btn">shop now</a>
         </div>
 
