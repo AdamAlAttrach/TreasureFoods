acadd8fffe17d12c47d3b86db7a06d6f2b220286
AdamAlAttrach <adamalattrach@gmail.com>
2022-10-23 21:55:36 -0400

    Finished all sections, now working on Cart

--- index.php
+++ index.php
@@ -14,37 +14,44 @@ if (isset($_SESSION["usersuid"])) {
 
         <div class="box">
             <img src="images/vegetables.jpg" alt="">
-            <h3>vegetables</h3>
+            <h3>Vegetables</h3>
             <p>Tomatoes are in this section</p>
-            <a href="#" class="btn">shop now</a>
+            <a href="vegetable.php" class="btn btn-info">Shop Now!</a>
         </div>
 
         <div class="box">
             <img src="images/fruits.jpg" alt="">
-            <h3>fresh fruits</h3>
+            <h3>Fruits</h3>
             <p>Have a taste of our delicious apples!</p>
-            <a href="#" class="btn">shop now</a>
+            <a href="fruits.php" class="btn btn-info">Shop Now!!</a>
         </div>
 
         <div class="box">
             <img src="images/cereal.jpg" alt="">
-            <h3>dairy products</h3>
+            <h3>Dairy</h3>
             <p>All part of a nutritious breakfast!</p>
-            <a href="#" class="btn">shop now</a>
+            <a href="dairy.php" class="btn btn-info">shop now</a>
         </div>
 
         <div class="box">
             <img src="images/Butcher.jpg" alt="">
-            <h3>fresh meat</h3>
+            <h3>Meat</h3>
             <p>For the meatlovers!</p>
-            <a href="#" class="btn">shop now</a>
+            <a href="meat.php" class="btn btn-info">Shop Now!</a>
         </div>
 
         <div class="box">
             <img src="images/winecellar.jpg" alt="">
             <h3>Captain's Wine Cellar</h3>
             <p>Get your own bottle of Caberlot before we run out!</p>
-            <a href="#" class="btn">shop now</a>
+            <a href="wine.php" class="btn btn-info">Shop Now!</a>
+        </div>
+
+        <div class="box">
+            <img src="images/sweets.jpg" alt="">
+            <h3>Treasure Trove</h3>
+            <p>Approach with vigilance</p>
+            <a href="wine.php" class="btn btn-info">Shop Now!</a>
         </div>
 
     </div>
