5f6482f8cba56bfad503c14c1a6fbf4533a64bb0
AdamAlAttrach <adamalattrach@gmail.com>
2022-10-31 19:07:41 -0400

    Fixed account database

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,8 @@
 <?php
 
-$serverName = "127.0.0.1";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "ecommerce_website";
+// Connect to DB
+$conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "b55e235a99ff55", "4dc93493", "heroku_f58f8abf2100592");
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
